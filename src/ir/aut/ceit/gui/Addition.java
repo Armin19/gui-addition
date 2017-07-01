168e3791c11e7cf59f654e0fddfd87e0f460c343
Armin19 <rmeen.shokati@gmail.com>
2017-07-01 23:11:33 +0430

    first commit

---
+++ src/ir/aut/ceit/gui/Addition.java
@@ -0,0 +1,18 @@
+/**
+ * package ir.aut.ceit.gui;
+ * <p>
+ * import javax.swing.*;
+ * <p>
+ * public class Addition {
+ * <p>
+ * <p>
+ * public static void main(String[] args) {
+ * String firstNumber = JOptionPane.showInputDialog("Enter first integer");
+ * String secondNumber = JOptionPane.showInputDialog("Enter second integer");
+ * int number1 = Integer.parseInt(firstNumber);
+ * int number2 = Integer.parseInt(secondNumber);
+ * int sum = number1 + number2;
+ * JOptionPane.showMessageDialog(null, "The sum is" + sum, "sum of two integers", JOptionPane.PLAIN_MESSAGE);
+ * }
+ * }
+ */
---
+++ src/ir/aut/ceit/gui/LabelTest.java
@@ -0,0 +1,14 @@
+/**package ir.aut.ceit.gui;
+
+import javax.swing.*;
+
+public class LabelTest {
+    public static void main(String[] args) {
+        LabelFrame labelFrame = new LabelFrame();
+        LabelFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        labelFrame.setSize(275, 180);
+        labelFrame.setVisible(true);
+
+    }
+}
+*/
